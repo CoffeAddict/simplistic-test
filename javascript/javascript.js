07de926b1904708d6177e2c019dbddfd4c6dee62
Andres Rodriguez <andresrodriguezesteban@gmail.com>
2020-07-12 10:03:38 -0300

    first commit

---
+++ javascript/javascript.js
@@ -0,0 +1,14 @@
+$(function(){
+
+	$('.main-carousel').slick({
+      autoplay: true,
+      dots: true,
+      arrows: true,
+      fade: true,
+      pauseOnHover: false,
+      speed: 1000,
+      prevArrow: '<i class="left-arrow lnr lnr-chevron-left-circle"></i>',
+      nextArrow: '<i class="left-arrow lnr lnr-chevron-right-circle"></i>'
+    });
+
+});
